d2779611ec9e82707322a33c6d91c4acf0be0832
alexgaoth <alexisobrenovic@gmail.com>
2025-12-18 15:58:38 -0800

    small fixes

--- main-section/src/data/nowData.js
+++ main-section/src/data/nowData.js
@@ -1,7 +1,7 @@
 export const getNowData = async () => {
   let data;
   try {
-    data = await fetchNowDataFromAPI("https://alexgaoth.com/now.json");
+    data = await fetchNowDataFromAPI("https://alexgaoth.com/profile.json");
   } catch (e) {
     data = {
       working: { "Error": "Failed to load working data" },
